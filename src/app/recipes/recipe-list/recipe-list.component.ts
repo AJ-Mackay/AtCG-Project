86e799c280d59b940c014f2e3d7fa7c5be95f668
Paul Mackay <paul.m.mackay85@gmail.com>
2023-02-21 13:06:09 +0000

    Outputting a List of Recipes with ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://bakesbybrownsugar.com/wp-content/uploads/2023/01/Cherry-Pie-Bars-3.jpg'),
     new Recipe('A Test Recipe', 'This is simply a test', 'https://bakesbybrownsugar.com/wp-content/uploads/2023/01/Cherry-Pie-Bars-3.jpg')
   ];
 }
