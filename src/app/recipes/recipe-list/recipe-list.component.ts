a9cf61c173be055af270cae5f7c15a823b87dfb7
Paul Mackay <paul.m.mackay85@gmail.com>
2023-02-23 15:15:37 +0000

    Passing Data with Event and Property Binding (Combined)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,8 +8,13 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://bakesbybrownsugar.com/wp-content/uploads/2023/01/Cherry-Pie-Bars-3.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://bakesbybrownsugar.com/wp-content/uploads/2023/01/Cherry-Pie-Bars-3.jpg')
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://bakesbybrownsugar.com/wp-content/uploads/2023/01/Cherry-Pie-Bars-3.jpg')
   ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
