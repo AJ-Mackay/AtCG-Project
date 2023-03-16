6120c40dc979e4c389320683aaaf2325b4650a9f
Paul Mackay <paul.m.mackay85@gmail.com>
2023-03-16 14:24:14 +0000

    Fetching Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
+import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({providedIn: 'root'})
@@ -12,5 +13,11 @@ export class DataStorageService {
         this.http.put('https://atcg-httpbackend-default-rtdb.europe-west1.firebasedatabase.app/recipes.json', recipes).subscribe(response => {
             console.log(response);
         });
+    }
+
+    fetchRecipes() {
+        this.http.get<Recipe[]>('https://atcg-httpbackend-default-rtdb.europe-west1.firebasedatabase.app/recipes.json').subscribe(recipes => {
+            this.recipeService.setRecipes(recipes);
+        })
     }
 }
