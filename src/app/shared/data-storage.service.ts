ec607090298882f1a24bddd9e236666d03a9afde
Paul Mackay <paul.m.mackay85@gmail.com>
2023-03-16 14:42:39 +0000

    Resolving Data Before Loading

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map } from "rxjs";
+import { map, tap } from "rxjs";
 
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
@@ -17,13 +17,13 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.http.get<Recipe[]>('https://atcg-httpbackend-default-rtdb.europe-west1.firebasedatabase.app/recipes.json').
+        return this.http.get<Recipe[]>('https://atcg-httpbackend-default-rtdb.europe-west1.firebasedatabase.app/recipes.json').
         pipe(map(recipes => {
             return recipes.map(recipe => {
                 return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
             });
-        })).subscribe(recipes => {
+        }), tap(recipes => {
             this.recipeService.setRecipes(recipes);
-        })
+        }));
     }
 }
