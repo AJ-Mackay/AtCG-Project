07b9006885aa93cbe5c2361829f2b200a3461706
Paul Mackay <paul.m.mackay85@gmail.com>
2023-03-20 17:29:39 +0000

    Attaching the Token with an Interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -18,11 +18,7 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.authService.user.pipe(take(1), exhaustMap(user => {
-            return this.http.get<Recipe[]>('https://atcg-httpbackend-default-rtdb.europe-west1.firebasedatabase.app/recipes.json', {
-                params: new HttpParams().set('auth', user.token)
-            })
-        }), map(recipes => {
+        return this.http.get<Recipe[]>('https://atcg-httpbackend-default-rtdb.europe-west1.firebasedatabase.app/recipes.json').pipe(map(recipes => {
             return recipes.map(recipe => {
                 return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
             });
