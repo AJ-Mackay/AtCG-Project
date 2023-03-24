733ea5267a389dfda0fdf75b9aa4eb7edc4b53b7
Paul Mackay <paul.m.mackay85@gmail.com>
2023-03-24 16:24:07 +0000

    Auth Finished (For Now...)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
-import { Subscription } from "rxjs";
+import { Subscription, map } from "rxjs";
+import { Store } from '@ngrx/store';
 
 import { AuthService } from "../auth/auth.service";
 import { DataStorageService } from "../shared/data-storage.service";
+import * as fromApp from '../store/app.reducer';
 
 @Component({
     selector: 'app-header',
@@ -12,10 +14,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
     isAuthenticated = false;
     private userSub: Subscription;
 
-    constructor(private dataStorageService: DataStorageService, private authService: AuthService) {}
+    constructor(private dataStorageService: DataStorageService, private authService: AuthService, private store: Store<fromApp.AppState>) {}
 
     ngOnInit() {
-        this.userSub = this.authService.user.subscribe(user => {
+        this.userSub = this.store.select('auth').pipe(map(authState => authState.user)).subscribe(user => {
             this.isAuthenticated = !!user;
         });
     }
